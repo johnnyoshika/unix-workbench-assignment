77bdd7f2be3da6d09758753bffba9eb895c0c60f
Johnny Oshika <johnnyoshika@gmail.com>
2018-01-24 23:19:40 -0800

    Refactor guessinggame.sh's procedure into smaller functions.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,33 +1,41 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+actual=$(ls -l | grep ^- | wc -l)
+
+function isNumeric() {
+    if [[ ! $1 =~ ^[0-9]+$ ]]
+    then
+        echo "'$1' is not a number. Try again..."
+        false
+    fi    
+}
+
+function isCorrect() {
+    if [[ $1 -eq $actual ]]
+    then
+        echo "'$1' is correct! Congratulations!!!"
+    else
+        false
+    fi
+}
+
+function giveHint() {
+    if [[ $1 -lt $actual ]]
+    then
+        echo $"'$1' is too low! Try again..."
+    else
+        echo $"'$1' is too high! Try again..."
+    fi    
+}
+
 while true
 do
-
+    echo ""
     echo "How many files are in the current directory?"
     read guess
-    actual=$(ls -l | grep ^- | wc -l)
 
-    if [[ ! $guess =~ ^[0-9]+$ ]]
-    then
-        echo "'$guess' is not a number. Try again..."
-        echo ""
-        continue
-    fi
-
-    if [[ $guess -eq $actual ]]
-    then
-        echo "'$guess' is correct!!!"
-        break
-    fi
-
-    if [[ $guess -lt $actual ]]
-    then
-        echo $"'$guess' is too low! Try again..."
-    else
-        echo $"'$guess' is too high! Try again..."
-    fi
-    
-    echo ""
-
+    ! isNumeric $guess && continue
+    isCorrect $guess && break
+    giveHint $guess
 done
