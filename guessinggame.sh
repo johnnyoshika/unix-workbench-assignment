f67322457e96a374c8ea60c2e2afd0506864742c
Johnny Oshika <johnnyoshika@gmail.com>
2018-01-24 17:26:26 -0800

    Add high/low hint to guessinggame.sh.

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,13 +8,26 @@ do
     read guess
     actual=$(ls -l | grep ^- | wc -l)
 
-    if [[ $guess =~ ^[0-9]+$ ]] && [[ $guess -eq $actual ]]
+    if [[ ! $guess =~ ^[0-9]+$ ]]
+    then
+        echo "'$guess' is not a number. Try again..."
+        echo ""
+        continue
+    fi
+
+    if [[ $guess -eq $actual ]]
     then
         echo "'$guess' is correct!!!"
         break
-    else
-        echo $"'$guess' is wrong! Try again..."
-        echo ""
     fi
+
+    if [[ $guess -lt $actual ]]
+    then
+        echo $"'$guess' is too low! Try again..."
+    else
+        echo $"'$guess' is too high! Try again..."
+    fi
+    
+    echo ""
 
 done
